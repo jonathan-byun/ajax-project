aae6839cf72e0fa8f1617c5a5abc4d17563901db
jonathan-byun <jonathanbyun1@gmail.com>
2022-03-01 16:30:46 -0800

    added fade transition to carousel

--- js/main.js
+++ js/main.js
@@ -12,14 +12,22 @@ xhr.send();
 
 xhr.addEventListener('load', startCarousel);
 var $introImg = document.querySelector('#intro-img');
-
+var $introImg2 = document.querySelector('#intro-img-2');
 function startCarousel() {
-  var IntroIntervalID = setInterval(switchImage, 1000);
+  var IntroIntervalID = setInterval(switchImage, 3000);
 }
 
 var currentImageNumber = 0;
 function switchImage() {
-  $introImg.src = ghibliMovies[currentImageNumber].image;
+  if (currentImageNumber % 2 === 0) {
+    $introImg.src = ghibliMovies[currentImageNumber].image;
+    $introImg2.classList.add('opacity-0');
+    $introImg.classList.remove('opacity-0');
+  } else {
+    $introImg2.src = ghibliMovies[currentImageNumber].image;
+    $introImg.classList.add('opacity-0');
+    $introImg2.classList.remove('opacity-0');
+  }
   currentImageNumber++;
   if (currentImageNumber === ghibliMovies.length) {
     currentImageNumber = 0;
