3d1a5fec85243f596809375499149ef2a8efafe1
jonathan-byun <jonathanbyun1@gmail.com>
2022-03-03 14:20:40 -0800

    import movite title poster and rating from api and local storage

--- js/main.js
+++ js/main.js
@@ -33,3 +33,49 @@ function switchImage() {
     currentImageNumber = 0;
   }
 }
+
+function makeNewMovieRow() {
+  var newMovieRow = document.createElement('div');
+  newMovieRow.className = 'row justify-around';
+  return newMovieRow;
+}
+
+function makeMoviePosterBlock(movie) {
+  var movieColumn = document.createElement('div');
+  movieColumn.className = 'column-one-fifth';
+  var posterContainer = document.createElement('div');
+  posterContainer.className = 'row align-center justify-between height-2rem';
+  movieColumn.appendChild(posterContainer);
+  var movieTitle = document.createElement('h2');
+  movieTitle.textContent = movie.title;
+  posterContainer.appendChild(movieTitle);
+  var rating = document.createElement('h3');
+  posterContainer.appendChild(rating);
+  var imageContainer = document.createElement('div');
+  imageContainer.className = 'row justify-center';
+  movieColumn.appendChild(imageContainer);
+  var moviePoster = document.createElement('img');
+  moviePoster.className = 'fit-to-container';
+  moviePoster.src = movie.image;
+  imageContainer.appendChild(moviePoster);
+  return movieColumn;
+}
+
+function populateFilmsList() {
+  var rowCounter = 0;
+  var $filmsContainer = document.querySelector('#films-container');
+  for (let i = 0; i < ghibliMovies.length; i++) {
+    if (rowCounter === 0) {
+      var newRow = makeNewMovieRow();
+    }
+    var newMovieColumn = makeMoviePosterBlock(ghibliMovies[i]);
+    newRow.appendChild(newMovieColumn);
+    rowCounter++;
+    if (rowCounter === 4) {
+      $filmsContainer.appendChild(newRow);
+      rowCounter = 0;
+    }
+  }
+}
+
+window.addEventListener('load', populateFilmsList);
